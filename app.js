749d7ed742cc84525fc6669c045b8b27ae34745f
Buff4eyes <claudemokbel@gmail.com>
2022-04-14 16:52:11 -0700

    api calls finished and styled the page

--- app.js
+++ app.js
@@ -1,32 +1,79 @@
+//  define multiple use variables
 jsonURL = 'https://jsonplaceholder.typicode.com'
 usersTable = document.getElementById('userTable');
 getUserButton = document.getElementById('getUsers');
-let users = []
+postSection = document.getElementById('userPosts')
+let posts = []
 
-const getUsers = async() => {
+// API CALLS
+// get users
+const getUsers = async () => {
   const endpoint = '/users'
   const urlToFetch = `${jsonURL}${endpoint}`
+
   try {
     const response = await fetch(urlToFetch);
     if (response.ok) {
+
       const jsonResponse = await response.json();
+      console.log(jsonResponse)
 
       for (let i = 0; i < jsonResponse.length; i++) {
-
-        var newRow = document.createElement("tr");
-        var newCell = document.createElement("td");
-        newCell.innerHTML = jsonResponse[i].name;
-          newRow.append(newCell);
-        document.getElementById("userTable").innerHTML(newRow)
+        // create a new row and data for each user name
+        let newRow = document.createElement("tr");
+        let newCell = document.createElement("td");
+        newCell.innerHTML = `${jsonResponse[i].name} <button class='postButton' data-key=${jsonResponse[i].id}>view posts</button>`;
+        // append the new data to the new row
+        newRow.append(newCell);
+        // append new row to the table
+        userTable.append(newRow)
       }
+      getUserButton.disabled = true;
+      return jsonResponse
     }
-    
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+// get posts
+const getPosts = async (key) => {
+  const endpoint = `/posts?userId=${key}`
+  const urlToFetch = `${jsonURL}${endpoint}`
+
+  try {
+    const response = await fetch(urlToFetch);
+    if (response.ok) {
+      const jsonResponse = await response.json()
+      posts = jsonResponse
+    }
   } catch(error) {
     console.log(error)
   }
 }
-
-getUserButton.addEventListener('click', event => { 
+// EVENT LISTENERS
+// get users button
+getUserButton.addEventListener('click', event => {
   getUsers();
 })
+// add event listener to document 
+document.addEventListener('click', event => {
+  const element = event.target
+  if (element.className === 'postButton') {
+    postSection.innerHTML = '';
+    getPosts(element.dataset.key)
+    for (let i=0; i < posts.length; i++) {
+      // create new elements for the dom
+      let title = document.createElement('h1')
+      let body = document.createElement('p')
+      //  add html
+      title.innerHTML = posts[i].title
+      body.innerHTML = posts[i].body
+      // append
+      title.append(body)
+      postSection.append(title)
+    }
 
+  }
+})
+ 
