ee2bf26ab8096674470a2fedd96a7e2b2a7fcac7
Buff4eyes <claudemokbel@gmail.com>
2022-04-14 20:29:41 -0700

    fixed double click issue

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ jsonURL = 'https://jsonplaceholder.typicode.com'
 usersTable = document.getElementById('userTable');
 getUserButton = document.getElementById('getUsers');
 postSection = document.getElementById('userPosts')
-let posts = []
+
 
 // API CALLS
 // get users
@@ -16,7 +16,7 @@ const getUsers = async () => {
     if (response.ok) {
 
       const jsonResponse = await response.json();
-      console.log(jsonResponse)
+
 
       for (let i = 0; i < jsonResponse.length; i++) {
         // create a new row and data for each user name
@@ -45,7 +45,17 @@ const getPosts = async (key) => {
     const response = await fetch(urlToFetch);
     if (response.ok) {
       const jsonResponse = await response.json()
-      posts = jsonResponse
+      for (let i = 0; i < jsonResponse.length; i++) {
+        // create new elements for the dom
+        let title = document.createElement('h1')
+        let body = document.createElement('p')
+        //  add html
+        title.innerHTML = jsonResponse[i].title
+        body.innerHTML = jsonResponse[i].body
+        // append
+        title.append(body)
+        postSection.append(title)
+      }
     }
   } catch(error) {
     console.log(error)
@@ -59,21 +69,9 @@ getUserButton.addEventListener('click', event => {
 // add event listener to document 
 document.addEventListener('click', event => {
   const element = event.target
-  if (element.className === 'postButton') {
+  if (element.className == 'postButton') {
     postSection.innerHTML = '';
     getPosts(element.dataset.key)
-    for (let i=0; i < posts.length; i++) {
-      // create new elements for the dom
-      let title = document.createElement('h1')
-      let body = document.createElement('p')
-      //  add html
-      title.innerHTML = posts[i].title
-      body.innerHTML = posts[i].body
-      // append
-      title.append(body)
-      postSection.append(title)
-    }
-
   }
 })
  
